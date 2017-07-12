6e174bfd543873036a89a690d215292f0145c5d2
Thomas Docu <thomas.docu0@gmail.com>
2017-07-12 19:35:38 -0400

    working on React routes and components - committing a few changes for now

--- src/components/PortalPage.js
+++ src/components/PortalPage.js
@@ -1,3 +1,4 @@
+// This will be the front page users reach when logging in or signing up.
 import React, { Component } from 'react';
 
 // React Link tags replace HTML <a> tags
--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
 // Set up your application entry point here...
+// Require React framework.
+import React from 'react';
+
+// Require the package to render the virtual Dom to real DOM.
+import ReactDom from 'react-dom';
+
+// Configure the router.
+import { Route, Router, browserHistory } from 'react-router';
+
+// Application assets like CSS will go here with import path
+import './assets/css/normalize.css';
+import './assets/css/style.css';
+
+// Import components here.
+import PortalPage from './components/PortalPage';
+
+ // ** These components are NOT created yet.
+import SignUp from './components/SignUp';
+import LogIn from './components/LogIn';
+import DashboardUser from './components/DashboardUser';
+import DashboardGear from './components/DashboardGear';
+import NewUser from './components/NewUser';
+import NewGear from './components/NewGear';
+import EditUser from './components/EditUser';
+import EditGear from './components/EditGear';
+
+// Restrict routes to logged in users only
+// const restrict = () => {
+//    if (!window.localStorage.getItem('token')) {
+//        browserHistory.push('/login');
+//    }
+// }
+
+// IMPERATIVE TO GET HELP WITH THESE ROUTES !!!!!!!!!
+ReactDom.render(
+  <Router history={browserHistory}>
+    <Route path='/' component={PortalPage} />
+    <Route path='/signup' component={SignUp} />
+    <Route path='/login' component={LogIn} />
+    <Route path='/gear/:user_id' component={DashGearSort} onEnter={restrict} />
+    <Route path='/user/:user_id/edit' component={EditUser} onEnter={restrict} />
+    <Route path='/gear/:user_id/gear/:id/edit' component={EditGear} onEnter={restrict} />
+    <Route path='/gear/:user_id/gear/new' component={NewGear} onEnter={restrict} />
+
+  </Router>
+, document.getElementById('app'));
