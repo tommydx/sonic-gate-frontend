2564617b823cc3bbb6ed0b3187108ec8ede9531d
Thomas Docu <thomas.docu0@gmail.com>
2017-07-12 13:22:21 -0400

    setting up React for front end development

---
+++ src/components/PortalPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// React Link tags replace HTML <a> tags
+import { Link } from 'react-router';
+
+class PortalPage extends Component {
+  constructor() {
+    super();
+  }
+}
+
+render() {
+  return()
+}
