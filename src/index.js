c1a93347f1aa07decbf18d7f2c6e90a00a671cb3
Thomas Docu <thomas.docu0@gmail.com>
2017-07-17 13:08:08 -0400

    troubleshooting new frontend - changed a few things

--- src/index.js
+++ src/index.js
@@ -38,10 +38,10 @@ ReactDom.render(
     <Route path='/' component={PortalPage} />
     <Route path='/signup' component={SignUp} />
     <Route path='/login' component={LogIn} />
-    // <Route path='/gear/:user_id' component={DashGearSort} onEnter={restrict} />
-    // <Route path='/user/:user_id/edit' component={EditUser} onEnter={restrict} />
-    // <Route path='/user/:user_id/gear/:id/edit' component={EditGear} onEnter={restrict} />
-    // <Route path='/user/:user_id/gear/new' component={NewGear} onEnter={restrict} />
+    <Route path='/gear/:user_id' component={DashGearSort} onEnter={restrict} />
+    <Route path='/user/:user_id/edit' component={EditUser} onEnter={restrict} />
+    <Route path='/user/:user_id/gear/:id/edit' component={EditGear} onEnter={restrict} />
+    <Route path='/user/:user_id/gear/new' component={NewGear} onEnter={restrict} />
 
   </Router>
 , document.getElementById('app'));
